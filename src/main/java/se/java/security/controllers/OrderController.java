21eb995084970b8e03d239ee01fce7c888fd06b7
Helena <helena@klarr.se>
2025-02-11 16:46:20 +0100

    added preparation for tomorrows class

---
+++ src/main/java/se/java/security/controllers/OrderController.java
@@ -0,0 +1,4 @@
+package se.java.security.controllers;
+
+public class OrderController {
+}
---
+++ src/main/java/se/java/security/services/ProductService.java
@@ -0,0 +1,43 @@
+package se.java.security.services;
+
+import org.springframework.stereotype.Service;
+import se.java.security.exceptions.ResourceNotFoundException;
+import se.java.security.models.Product;
+import se.java.security.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product) {
+        if (product.getName() == null || product.getName().isEmpty()) {
+            throw new IllegalArgumentException("Product name cannot be empty");
+        }
+        if (product.getPrice() == null || product.getPrice() <= 0) {
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(String id) {
+        return productRepository.findById(id);
+    }
+
+    public void deleteProduct(String id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + id));
+
+        productRepository.delete(product);
+    }
+}
